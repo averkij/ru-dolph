eeab4b9b433672c1f8d6c9b74981727fe80c5c9e
Sergei Averkiev <averoo@gmail.com>
2022-11-16 11:13:51 +0300

    Black formatting.

--- rudolph/model/model.py
+++ rudolph/model/model.py
@@ -8,35 +8,39 @@ from .transformer import SparseTransformer
 
 
 class ruDolphModel(torch.nn.Module):
-    def __init__(self,
-                 device,
-                 num_layers,
-                 vocab_size,
-                 hidden_size,
-                 num_attention_heads,
-                 embedding_dropout_prob,
-                 attention_dropout_prob,
-                 output_dropout_prob,
-                 l_text_seq_length=64,
-                 r_text_seq_length=64,
-                 kernel_size=7,
-                 last_kernel_size=9,
-                 image_tokens_per_dim=16,
-                 image_vocab_size=8192,
-                 text_special_tokens=0,
-                 image_special_tokens=0,
-                 cogview_sandwich_layernorm=True,
-                 cogview_pb_relax=True,
-                 is_bool_mask=True,
-                 mlp_activation='gelu_jit',
-                 gradient_checkpointing=None):
+    def __init__(
+        self,
+        device,
+        num_layers,
+        vocab_size,
+        hidden_size,
+        num_attention_heads,
+        embedding_dropout_prob,
+        attention_dropout_prob,
+        output_dropout_prob,
+        l_text_seq_length=64,
+        r_text_seq_length=64,
+        kernel_size=7,
+        last_kernel_size=9,
+        image_tokens_per_dim=16,
+        image_vocab_size=8192,
+        text_special_tokens=0,
+        image_special_tokens=0,
+        cogview_sandwich_layernorm=True,
+        cogview_pb_relax=True,
+        is_bool_mask=True,
+        mlp_activation="gelu_jit",
+        gradient_checkpointing=None,
+    ):
         super(ruDolphModel, self).__init__()
         self.device = device
         self.image_tokens_per_dim = image_tokens_per_dim
-        self.image_seq_length = image_tokens_per_dim ** 2
+        self.image_seq_length = image_tokens_per_dim**2
         self.l_text_seq_length = l_text_seq_length
         self.r_text_seq_length = r_text_seq_length
-        self.total_seq_length = self.l_text_seq_length + self.image_seq_length + self.r_text_seq_length
+        self.total_seq_length = (
+            self.l_text_seq_length + self.image_seq_length + self.r_text_seq_length
+        )
         self.text_special_tokens = text_special_tokens
         self.image_special_tokens = image_special_tokens
         vocab_size = vocab_size + text_special_tokens
@@ -52,10 +56,18 @@ class ruDolphModel(torch.nn.Module):
         self.image_embeddings = torch.nn.Embedding(image_vocab_size, hidden_size)
 
         # Position embedding (serial).
-        self.l_text_pos_embeddings = torch.nn.Embedding(l_text_seq_length + 1, hidden_size)
-        self.r_text_pos_embeddings = torch.nn.Embedding(r_text_seq_length + 1, hidden_size)
-        self.image_row_embeddings = torch.nn.Embedding(image_tokens_per_dim, hidden_size)
-        self.image_col_embeddings = torch.nn.Embedding(image_tokens_per_dim, hidden_size)
+        self.l_text_pos_embeddings = torch.nn.Embedding(
+            l_text_seq_length + 1, hidden_size
+        )
+        self.r_text_pos_embeddings = torch.nn.Embedding(
+            r_text_seq_length + 1, hidden_size
+        )
+        self.image_row_embeddings = torch.nn.Embedding(
+            image_tokens_per_dim, hidden_size
+        )
+        self.image_col_embeddings = torch.nn.Embedding(
+            image_tokens_per_dim, hidden_size
+        )
         init_method(self.l_text_pos_embeddings.weight)
         init_method(self.r_text_pos_embeddings.weight)
         init_method(self.image_row_embeddings.weight)
@@ -93,13 +105,25 @@ class ruDolphModel(torch.nn.Module):
     def get_image_pos_embeddings(self, image_input_ids, device, past_length=0):
         input_shape = image_input_ids.size()
         row_ids = torch.div(
-            torch.arange(past_length, input_shape[-1] + past_length, dtype=torch.long, device=self.device),
+            torch.arange(
+                past_length,
+                input_shape[-1] + past_length,
+                dtype=torch.long,
+                device=self.device,
+            ),
             self.image_tokens_per_dim,
-            rounding_mode='trunc'
+            rounding_mode="trunc",
         )
         row_ids = row_ids.unsqueeze(0).view(-1, input_shape[-1])
-        col_ids = torch.arange(past_length, input_shape[-1] + past_length,
-                               dtype=torch.long, device=device) % self.image_tokens_per_dim
+        col_ids = (
+            torch.arange(
+                past_length,
+                input_shape[-1] + past_length,
+                dtype=torch.long,
+                device=device,
+            )
+            % self.image_tokens_per_dim
+        )
         col_ids = col_ids.unsqueeze(0).view(-1, input_shape[-1])
         return self.image_row_embeddings(row_ids) + self.image_col_embeddings(col_ids)
 
@@ -116,12 +140,14 @@ class ruDolphModel(torch.nn.Module):
         return_hidden_states=False,
     ):
         device = input_ids.device
-        l_text = input_ids[:, :self.l_text_seq_length]
+        l_text = input_ids[:, : self.l_text_seq_length]
         l_text_range = torch.arange(l_text.shape[1])
-        l_text_range += (self.vocab_size - self.l_text_seq_length)
+        l_text_range += self.vocab_size - self.l_text_seq_length
         l_text_range = l_text_range.to(device)
         l_text = torch.where(l_text == 0, l_text_range, l_text)
-        l_text_pos = self.l_text_pos_embeddings(torch.arange(l_text.shape[1], device=device))
+        l_text_pos = self.l_text_pos_embeddings(
+            torch.arange(l_text.shape[1], device=device)
+        )
         l_text_embeddings = self.text_embeddings(l_text) + l_text_pos
 
         use_image = input_ids.shape[1] > self.l_text_seq_length
@@ -129,14 +155,21 @@ class ruDolphModel(torch.nn.Module):
 
         embeddings = [l_text_embeddings]
         if use_image:
-            image_input_ids = input_ids[:, self.l_text_seq_length:self.l_text_seq_length + self.image_seq_length]
-            img_pos = self.get_image_pos_embeddings(image_input_ids, past_length=0, device=device)
+            image_input_ids = input_ids[
+                :,
+                self.l_text_seq_length : self.l_text_seq_length + self.image_seq_length,
+            ]
+            img_pos = self.get_image_pos_embeddings(
+                image_input_ids, past_length=0, device=device
+            )
             image_embeddings = self.image_embeddings(image_input_ids) + img_pos
             embeddings.append(image_embeddings)
 
         if use_r_text:
-            r_text = input_ids[:, self.l_text_seq_length + self.image_seq_length:]
-            r_text_pos = self.r_text_pos_embeddings(torch.arange(r_text.shape[1], device=device))
+            r_text = input_ids[:, self.l_text_seq_length + self.image_seq_length :]
+            r_text_pos = self.r_text_pos_embeddings(
+                torch.arange(r_text.shape[1], device=device)
+            )
             r_text_embeddings = self.text_embeddings(r_text) + r_text_pos
             embeddings.append(r_text_embeddings)
 
@@ -145,10 +178,15 @@ class ruDolphModel(torch.nn.Module):
         alpha = 0.1
         embeddings = embeddings * alpha + embeddings.detach() * (1 - alpha)
 
-        attention_mask = attention_mask[:, :, :embeddings.shape[1], :embeddings.shape[1]]
+        attention_mask = attention_mask[
+            :, :, : embeddings.shape[1], : embeddings.shape[1]
+        ]
         transformer_output, present_cache, hidden_states = self.transformer(
-            embeddings, attention_mask, cache=cache, use_cache=use_cache,
-            gradient_checkpointing=self.gradient_checkpointing
+            embeddings,
+            attention_mask,
+            cache=cache,
+            use_cache=use_cache,
+            gradient_checkpointing=self.gradient_checkpointing,
         )
 
         logits = self.to_logits(transformer_output)
@@ -158,45 +196,62 @@ class ruDolphModel(torch.nn.Module):
                 outputs += (hidden_states,)
             return outputs
 
-        logits = rearrange(logits, 'b n c -> b c n')
-        l_text_logits = logits[
-            :, :self.vocab_size, :self.l_text_seq_length if use_image else self.l_text_seq_length-1
-        ].contiguous().float()
+        logits = rearrange(logits, "b n c -> b c n")
+        l_text_logits = (
+            logits[
+                :,
+                : self.vocab_size,
+                : self.l_text_seq_length if use_image else self.l_text_seq_length - 1,
+            ]
+            .contiguous()
+            .float()
+        )
         labels = [l_text[:, 1:]]
         if use_image:
             labels.append(image_input_ids)
-            a, b = self.l_text_seq_length, self.l_text_seq_length + self.image_seq_length - 1
-            image_logits = logits[:, self.vocab_size:, a:b].contiguous().float()
+            a, b = (
+                self.l_text_seq_length,
+                self.l_text_seq_length + self.image_seq_length - 1,
+            )
+            image_logits = logits[:, self.vocab_size :, a:b].contiguous().float()
         if use_r_text:
-            r_text_logits = logits[:, :self.vocab_size, -self.r_text_seq_length:-1].contiguous().float()
+            r_text_logits = (
+                logits[:, : self.vocab_size, -self.r_text_seq_length : -1]
+                .contiguous()
+                .float()
+            )
             labels.append(r_text)
         labels = torch.cat(labels, dim=1).contiguous().long()
 
         loss, loss_weights, loss_values = 0, 0, {}
         loss_l_text = F.cross_entropy(
-            l_text_logits,
-            labels[:, :self.l_text_seq_length]
+            l_text_logits, labels[:, : self.l_text_seq_length]
         )
-        loss_values['l_text_loss'] = loss_l_text.data.detach().float()
+        loss_values["l_text_loss"] = loss_l_text.data.detach().float()
         if lt_loss_weight:
-            loss += loss_l_text*lt_loss_weight
+            loss += loss_l_text * lt_loss_weight
             loss_weights += lt_loss_weight
         if use_image:
             loss_img = F.cross_entropy(
                 image_logits,
-                labels[:, self.l_text_seq_length:self.l_text_seq_length + self.image_seq_length - 1]
+                labels[
+                    :,
+                    self.l_text_seq_length : self.l_text_seq_length
+                    + self.image_seq_length
+                    - 1,
+                ],
             )
-            loss_values['image_loss'] = loss_img.data.detach().float()
+            loss_values["image_loss"] = loss_img.data.detach().float()
             if img_loss_weight:
-                loss += loss_img*img_loss_weight
+                loss += loss_img * img_loss_weight
                 loss_weights += img_loss_weight
         if use_r_text:
             loss_r_text = F.cross_entropy(
                 r_text_logits,
-                labels[:, -(self.r_text_seq_length-1):],
+                labels[:, -(self.r_text_seq_length - 1) :],
                 ignore_index=0,
             )
-            loss_values['r_text_loss'] = loss_r_text.data.detach().float()
+            loss_values["r_text_loss"] = loss_r_text.data.detach().float()
             if rt_loss_weight:
                 loss += loss_r_text * rt_loss_weight
                 loss_weights += rt_loss_weight
